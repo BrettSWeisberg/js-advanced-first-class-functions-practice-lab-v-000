295f3ea76a3a99c6a92c2de897bd803ceb269382
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-18 17:38:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+
+ function logDriverNames(drivers) {
+   for each (name item in drivers) {
+  // return drivers.forEach(function(name) {
+     return console.log(name)
+}
+}
